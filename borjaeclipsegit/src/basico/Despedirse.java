045ea18ff1fafdd24adf9e95bb81be63ab24d103
Curso mañana <Curso maana@PC01ra5>
2018-01-29 10:21:45 +0100

    Prueba dos

---
+++ borjaeclipsegit/src/basico/Despedirse.java
@@ -0,0 +1,10 @@
+package basico;
+
+public class Despedirse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Adios!! Mañana mas y mejor.");
+	}
+
+}
